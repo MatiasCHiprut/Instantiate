7cf05048f631e5379eb1854c8c5ccd9ec07d1102
MatiasChiprut <92793353+MatiasCHiprut@users.noreply.github.com>
2022-05-09 12:04:55 -0300

    Ejercicio terminado

---
+++ Assets/Scripts/Instatiator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instatiator : MonoBehaviour
+{
+    public GameObject ObjectToClone;
+    public int maximo;
+    public void CloneObj()
+    {
+        int contador=0;
+        while (contador <= maximo)
+        {
+            Instantiate(ObjectToClone);
+            contador++;
+        }
+    }
+}
